c831dcd5fb137fbdff1e6459965be2f4480f5a54
harshadjaybhay301 <harshadjaybhay301@gmail.com>
2024-07-17 15:50:36 +0530

    Initial Commit

---
+++ TechBuzzBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace TechBuzzBackend.Data
+using Microsoft.EntityFrameworkCore;
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+    }
+}
